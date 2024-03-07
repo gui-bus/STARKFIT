af65974f2b57cd960847e5cf76e31b21456d9130
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-03-07 11:41:37 -0300

    feat: add shadcn carousel

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -13,14 +13,17 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-muted-foreground/10 hover:text-accent-foreground hover:scale-[1.01] transition-all duration-300 ease-in",
+          "border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        shadow: "bg-primary text-primary-foreground hover:bg-primary/90 shadow-lg shadow-primary hover:scale-[1.01] transition-all duration-300 ease-in",
-        bordered: "bg-transparent text-primary border border-primary hover:bg-primary hover:text-white transition-all duration-300 ease-in",
-        borderedDark: "bg-transparent text-black border border-black/20 hover:bg-[#1A1A1A] hover:text-white transition-all duration-200 ease-in"
+        shadow:
+          "bg-primary text-primary-foreground hover:bg-primary/90 shadow-md shadow-primary hover:scale-[1.01] transition-all duration-300 ease-in text-white",
+        bordered:
+          "bg-transparent text-primary border border-primary hover:bg-primary hover:text-white transition-all duration-300 ease-in",
+        borderedDark:
+          "bg-transparent text-black border border-black/20 hover:bg-[#1A1A1A] hover:text-white transition-all duration-200 ease-in",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -34,26 +37,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
