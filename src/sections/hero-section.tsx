1689a3e03ea19f356235e70db089062c6fe139b8
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-03-07 19:24:40 -0300

    feat: change hero section banner

--- src/sections/hero-section.tsx
+++ src/sections/hero-section.tsx
@@ -1,60 +1,46 @@
 import { Button } from "@nextui-org/react";
-import Image from "next/image";
 import Link from "next/link";
 import { FaStar } from "react-icons/fa6";
 import { SiVitest } from "react-icons/si";
 
 const HeroSection = () => {
   return (
-    <section>
-      <div className="flex flex-col xl:flex-row items-center justify-between gap-5 maxContainer">
-        <Image
-          alt="Hero"
-          className="mx-auto overflow-hidden 3xl:rounded-b-2xl xl:rounded-br-2xl object-cover w-full xl:w-1/2 h-auto drop-shadow-xl"
-          height={0}
-          src="/HeroBanner.svg"
-          width={0}
-          sizes="100vw"
-        />
+    <section className="bg-[url('/HeroBanner.svg')] bg-cover bg-center bg-no-repeat py-10 lg:py-20 3xl:rounded-b-2xl">
+      <div className="flex flex-col items-center justify-center gap-5 text-white w-full max-w-5xl mx-auto text-center p-5">
+        <h1 className="lg:leading-tighter font-black text-5xl lg:text-8xl uppercase flex-1 tracking-tighter">
+          Energize o seu estilo de vida
+        </h1>
+        <p className="font-light flex-1 max-w-4xl">
+          Na SparkFit, não somos apenas uma academia, somos o combustível para o
+          seu bem-estar. Estamos aqui para impulsionar seu estilo de vida,
+          oferecendo mais do que apenas um lugar para treinar.
+        </p>
 
-        <div className="flex flex-col items-center justify-center md:px-10 text-center px-5 gap-10 xl:w-1/2">
-          <div className="flex flex-col items-center justify-center gap-5">
-            <h1 className="lg:leading-tighter font-black tracking-tighter text-4xl lg:text-6xl uppercase flex-1">
-            Energize o seu estilo de vida
-            </h1>
-            <p className="mx-auto font-light flex-1">
-              Na SparkFit, não somos apenas uma academia, somos o combustível
-              para o seu bem-estar. Estamos aqui para impulsionar seu estilo de
-              vida, oferecendo mais do que apenas um lugar para treinar.
-            </p>
-          </div>
+        <div className="flex flex-col xl:flex-row items-center justify-center gap-5 w-full">
+          <Link href="#" className="w-full">
+            <Button
+              variant={"bordered"}
+              size={"lg"}
+              className="h-14 w-full uppercase text-white"
+              endContent={<SiVitest size={20} />}
+              radius="sm"
+            >
+              Experimente uma Aula
+            </Button>
+          </Link>
 
-          <div className="flex flex-col lg:flex-row items-center justify-center gap-5 w-full">
-            <Link href="#" className="w-full">
-              <Button
-                variant={"bordered"}
-                size={"lg"}
-                className="h-14 w-full uppercase"
-                endContent={<SiVitest size={20} />}
-                radius="sm"
-              >
-                Experimente uma Aula
-              </Button>
-            </Link>
-
-            <Link href="#" className="w-full">
-              <Button
-                variant={"shadow"}
-                color="warning"
-                size={"lg"}
-                className="h-14 w-full uppercase text-white"
-                endContent={<FaStar size={20} />}
-                radius="sm"
-              >
-                Conheça Nossos Planos
-              </Button>
-            </Link>
-          </div>
+          <Link href="#" className="w-full">
+            <Button
+              variant={"shadow"}
+              color="warning"
+              size={"lg"}
+              className="h-14 w-full uppercase text-white"
+              endContent={<FaStar size={20} />}
+              radius="sm"
+            >
+              Conheça Nossos Planos
+            </Button>
+          </Link>
         </div>
       </div>
     </section>
