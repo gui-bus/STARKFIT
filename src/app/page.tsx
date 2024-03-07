19c18ef6451764178683a2c7828fe5b0c1107784
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-03-07 19:26:18 -0300

    style: remove divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Separator } from "@/components/ui/separator";
 import AboutSection from "@/sections/about-section";
 import BenefitsSection from "@/sections/benefits-section";
 import ContactSection from "@/sections/contact-section";
 import FreeTraining from "@/sections/free-training";
 import HeroSection from "@/sections/hero-section";
 import MembershipSection from "@/sections/membership-section";
+import { Divider } from "@nextui-org/react";
 
 export default function Home() {
   return (
@@ -12,6 +12,8 @@ export default function Home() {
       <HeroSection />
 
       <BenefitsSection />
+
+      <Divider className="my-10 w-full max-w-[90%] mx-auto"/>
 
       <AboutSection />
 
