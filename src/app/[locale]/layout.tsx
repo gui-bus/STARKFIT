34f572e2e3d38d305fdc8b00f3b6ecd9af6f2970
Guilherme Bustamante <guilherme_bustamante@live.com>
2026-03-20 21:49:11 -0300

    fix: correct params type in layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,10 @@
 import { Noto_Sans } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import { routing } from "./i18n/routing";
+import { routing } from "../../i18n/routing";
 import { notFound } from "next/navigation";
 
 const montserrat = Noto_Sans({ subsets: ["latin"], weight: ["300", "400", "500", "700", "900"] });
@@ -37,10 +37,12 @@ export default async function RootLayout({
     <html lang={locale}>
       <body className={`${montserrat.className} w-full max-w-440 mx-auto bg-background px-4 md:px-10`}>
         <NextIntlClientProvider messages={messages} locale={locale}>
-          <Header />
-          <main className="flex-1 w-full max-w-440 mx-auto">{children}</main>
-          <Footer />
-        </NextIntlProvider>
+          <>
+            <Header />
+            <main className="flex-1 w-full max-w-440 mx-auto">{children}</main>
+            <Footer />
+          </>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
